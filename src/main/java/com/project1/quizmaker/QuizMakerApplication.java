d2ce1d06528ba750fb6b3894681674bc6e8fc435
SamahZughaibi <sama7.1999@gmail.com>
2023-11-20 23:33:17 +0300

    add cors cofigurer

--- src/main/java/com/project1/quizmaker/QuizMakerApplication.java
+++ src/main/java/com/project1/quizmaker/QuizMakerApplication.java
@@ -2,9 +2,21 @@ package com.project1.quizmaker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class QuizMakerApplication {
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry){
+				registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
+			}
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuizMakerApplication.class, args);
